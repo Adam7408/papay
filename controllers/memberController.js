936523dd58ff11ffd8fb8b99ca6828c5fe43b284
Adam <yusufabuali74@gmail.com>
2023-10-13 19:50:32 +0900

    feat: member controllerlarni qurish

---
+++ controllers/memberController.js
@@ -0,0 +1,18 @@
+let memberController = module.exports;
+
+memberController.home=(req,res) =>{
+    console.log("GET browserdagi homega ogri kirdi");
+    res.send("home sahifadasiz");
+};
+memberController.signup=(req,res) =>{
+    console.log("POST cont.signup");
+    res.send("signup sahifadasiz");
+};
+memberController.login=(req,res) =>{
+    console.log("POST cont.login");
+    res.send("login sahifadasiz");
+};
+memberController.logout=(req,res) =>{
+    console.log("GET cont.logout");
+    res.send("logout sahifadasiz");
+};
--- router.js
+++ router.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const memberController=require('./controllers/memberController');
 
-router.get("/",function(req,res){
-    res.send("home sahifadasiz");
-});
-
+// memberga dahildor routerlar
+router.get("/",memberController.home);
+router.post("/signup",memberController.signup);
+router.post("/login",memberController.login);
+router.get("/logout",memberController.logout);
+// boshqa routerlar
 router.get("/menu",(req,res)=>{
     res.send("Menu sahifadasiz");
 });
